17a744e30ddf00c2d2eab6aa325c6c711c3f2a15
angelvalverde21 <angelvalverde21@gmail.com>
2025-10-15 11:45:15 -0500

    route add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/cache', function () {
+
+    Artisan::call('cache:clear');
+    Artisan::call('config:cache');
+    Artisan::call('route:cache');
+    Artisan::call('view:cache');
+
+    return "Cacheado!x";
+});
+
+Route::get('/link', function () {
+    Artisan::call('storage:link');
 });
