74d133a73bc359677b49349bd52618912b72323c
angel valverde <angelvalverde21@gmail.com>
2025-11-18 16:03:47 -0500

    x

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,22 +9,15 @@ use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // $middleware->append(ForceJsonUnauthenticated::class);
-        //
-        // $middleware->append(VerifyStore::class);
-        // $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class); //no hemos logrado activar cors 
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-        $exceptions->renderable(function (\Illuminate\Auth\AuthenticationException $e, $request) {
-            return response()->json([
-                'message' => 'Unauthenticated.'
-            ], 401);
-        });
+        
     })->create();
