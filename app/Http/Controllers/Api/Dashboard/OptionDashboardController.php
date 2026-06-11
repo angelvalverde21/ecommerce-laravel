f61241115d092e8ba4162424817c5e015c293a4e
angel valverde <angelvalverde21@gmail.com>
2026-06-10 19:24:40 -0500

    x

--- app/Http/Controllers/Api/Dashboard/OptionDashboardController.php
+++ app/Http/Controllers/Api/Dashboard/OptionDashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Models\Option;
-use App\Models\store;
+use App\Models\Store;
 use App\Services\Dashboard\Option\OptionService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
