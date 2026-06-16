86ed309ac6e5223871f1311db4c73d4ee995d0fc
angel valverde <angelvalverde21@gmail.com>
2026-06-16 13:52:33 -0500

    x

--- app/Models/Manufacture.php
+++ app/Models/Manufacture.php
@@ -48,7 +48,7 @@ class Manufacture extends Model
 
     public function payments()
     {
-        return $this->morphMany(Payment::class, 'paymentable');
+        return $this->morphMany(Payment::class, 'paymentable')->orderBy('date', 'desc')
     }
 
     public function kardexes()
