b12568d87dfbdb47b86b63e4817e76632198dda9
Angel Valverde <angelvalverde21@gmail.com>
2026-08-06 09:16:21 -0500

    x

--- app/Models/Acquire.php
+++ app/Models/Acquire.php
@@ -18,11 +18,15 @@ class Acquire extends Model
     public function supplier()
     {
         return $this->belongsTo(Supplier::class);
-    }   
+    }
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    public function batches()
+    {
+        return $this->morphMany(Batch::class, 'batchable');
+    }
 }
